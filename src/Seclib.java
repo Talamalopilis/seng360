0c6aef9360ffd50a48bdf27f5b06338715522100
ChaseAPorter <chaseaporter@gmail.com>
2017-11-08 23:47:25 -0800

    In-Progress Crypto

--- src/Seclib.java
+++ src/Seclib.java
@@ -1,12 +1,34 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Scanner;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.*;
+import java.lang.StringBuffer;
 
 /**
  * Created by Tal on 11/8/2017.
  */
 
 public class Seclib {
+
+	//Note that the next three lines simulate the previous sharing of a key between the Server and Client users
+	public static String encodedKey = "abcdefghijklmnop";
+	public static byte[] decodedKey = Base64.getDecoder().decode(encodedKey);
+	public static Key sKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");	
 
     public static String messageHash(String message) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
@@ -96,11 +118,56 @@ public class Seclib {
                 System.out.println();
             }
         }
+		
+		//create security string
 
         for(int i = 0; i < 3; i++){
             securityArrayString = securityArrayString + String.valueOf(securityArray[i]);
         }
+		
+		//Use security array values to create key, etc based on need
+		
+		if(securityArray[0] == 1){
+			System.out.println("C operations needed");
+		}
+		
+		if(securityArray[1] == 1){
+			System.out.println("I operations needed");
+		}
+
+		if(securityArray[2] == 1){
+			System.out.println("A operations needed");
+		}		
 
         return securityArrayString;
     }
+	
+	public static byte[] encryptMessage(String plainText)throws Exception{
+		try{
+		
+			System.out.println("encryptMessage() called");
+			Cipher cipher = null;
+		
+			cipher.init(Cipher.ENCRYPT_MODE, sKey);
+			byte[] plainTextBytes = plainText.getBytes();
+			byte[] encryptedBytes = cipher.doFinal(plainTextBytes);
+			System.out.println("Printing the cipher: "+encryptedBytes);			
+			return encryptedBytes;
+
+		} catch(Exception e){
+				System.out.println("There was an issue with the encryption that called an exception");
+		  return null;}
+	}
+	
+    public static String decryptMessage(byte[] encryptedBytes) throws Exception {
+		try{
+			Cipher cipher = null;
+	
+			cipher.init(Cipher.DECRYPT_MODE, sKey);
+			byte[] recoveredBytes = cipher.doFinal(encryptedBytes);
+			String decryptionString = new String(recoveredBytes);
+		
+			return decryptionString;
+		} catch(Exception e){return null;}	
+    }	
 }
