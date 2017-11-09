1e02fa3f5ccee60f7f0b8d9b211f68ac91b9fe7c
Talamalopilis <tal.melamed.69@gmail.com>
2017-11-09 01:51:15 -0800

    cipher test

---
+++ src/test.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Tal on 11/9/2017.
+ */
+public class test {
+
+    public static void main(String args[]) throws Exception {
+        String text = "hi asdf asdf a asdf af e eaf es fe esfse esf s";
+        byte[] enc = Seclib.encryptMessage(text);
+        System.out.println(enc);
+        String dec = Seclib.decryptMessage(enc);
+        System.out.println(dec);
+    }
+}
